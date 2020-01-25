dcc5baf9f9fae6038e7c370c72b76ccd436c1eea
frasza <zan.fraas@gmail.com>
2020-01-25 16:26:25 +0100

    Init commit

---
+++ lib/widget_spinner.dart
@@ -0,0 +1,15 @@
+library widget_spinner;
+
+import 'package:flutter/material.dart';
+
+class WidgetSpinner extends StatefulWidget {
+  @override
+  _WidgetSpinnerState createState() => _WidgetSpinnerState();
+}
+
+class _WidgetSpinnerState extends State<WidgetSpinner> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
