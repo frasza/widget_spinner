8f641ca9678f7d50b789f511269dc8fb94a30e5a
frasza <zan.fraas@gmail.com>
2020-01-26 10:36:46 +0100

    Make spinner work and set properties

--- lib/widget_spinner.dart
+++ lib/widget_spinner.dart
@@ -3,13 +3,46 @@ library widget_spinner;
 import 'package:flutter/material.dart';
 
 class WidgetSpinner extends StatefulWidget {
+  final Widget child;
+  final double turns;
+  final int duration;
+
+  WidgetSpinner({Key key, this.child, this.turns = 10, this.duration = 10})
+      : super(key: key);
+
   @override
   _WidgetSpinnerState createState() => _WidgetSpinnerState();
 }
 
-class _WidgetSpinnerState extends State<WidgetSpinner> {
+class _WidgetSpinnerState extends State<WidgetSpinner>
+    with TickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        duration: Duration(seconds: widget.duration), vsync: this);
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOutSine,
+    );
+  }
+
+  void onTap() {
+    _animationController.reset();
+    _animationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: RotationTransition(
+        turns: Tween<double>(begin: 0, end: widget.turns).animate(_animation),
+        child: widget.child,
+      ),
+    );
   }
 }
